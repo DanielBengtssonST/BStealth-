0bda56eace6b0b332a54895b59e8427a963176d5
DanielBengtssonST <b_leinad@hotmail.com>
2018-12-11 16:37:45 +0100

    Spinning detection
    Added a simple spin script to enemy detection.

---
+++ BStealth/Assets/Scripts/DanielsScripts/SimpleRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRotation : MonoBehaviour {
+
+	public bool rotating;
+	public float XRotation, YRotation, ZRotation;
+
+
+	// Update is called once per frame
+	void Update () {
+	
+		if (rotating){
+
+			transform.Rotate (XRotation, YRotation, ZRotation);
+		}
+	}
+}
