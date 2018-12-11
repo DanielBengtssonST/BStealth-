ab28437ba93c9a1b6b2c32d49df09aad05727773
OskarMagnusson <oskar.magnusson97@gmail.com>
2018-12-11 22:42:32 +0100

    AI förljer spelare
    Den kan gå igenom väggar dock

--- BStealth/Assets/Scripts/OskarsScripts/AImovement.cs
+++ BStealth/Assets/Scripts/OskarsScripts/AImovement.cs
@@ -9,27 +9,38 @@ public class AImovement : MonoBehaviour {
     Transform[] wayPoints; //Agent moves to waypoint in order
     int destPoint = 0;
     NavMeshAgent agent;
+    [SerializeField]
+    bool foundPlayer = false;
+    [SerializeField]
+    GameObject thePlayer;
 
 	// Use this for initialization
 	void Start () {
         agent = GetComponent<NavMeshAgent>();
         agent.autoBraking = false;
 
-        GoToNextPoint();
+        GoToNextWayPoint();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (!agent.pathPending && agent.remainingDistance < 0.5f)
-            GoToNextPoint();
+        if (!agent.pathPending && agent.remainingDistance < 0.5f && foundPlayer == false)
+            GoToNextWayPoint();
+        else if(foundPlayer == true)
+            GoToPlayer();
 	}
 
-    void GoToNextPoint()
+    void GoToNextWayPoint()
     {
         if (wayPoints.Length != 0)
         {
             agent.destination = wayPoints[destPoint].position; //moves to the next waypoint
             destPoint = (destPoint + 1) % wayPoints.Length; //Cykles through the array
         }
+    }
+
+    void GoToPlayer()
+    {
+        agent.destination = new Vector3(thePlayer.transform.position.x, 0, thePlayer.transform.position.z);
     }
 }
