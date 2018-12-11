0c76aed6d10fd9757201f90b4918cbfc4c9cd9c0
Andreas Halldin <iqed@live.se>
2018-12-11 13:17:38 +0100

    Sight Cones

---
+++ BStealth/Assets/Scripts/EnemySight.cs
@@ -0,0 +1,200 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySight : MonoBehaviour
+{
+    public float viewRadius;
+    [Range(0, 360)]
+    public float viewAngle;
+
+
+    public LayerMask obstacleMask;
+    public LayerMask playerMask;
+
+    public float meshResolution;
+    public int edgeResolveIterations;
+    public float edgeDstThreshold;
+
+
+    public MeshFilter viewMeshFilter;
+    Mesh viewMesh;
+
+    private void Start()
+    {
+        viewMesh = new Mesh();
+        viewMesh.name = "View Mesh";
+        viewMeshFilter.mesh = viewMesh;
+        StartCoroutine("FindPlayerWithDelay", .2f);
+    }
+
+    private void LateUpdate()
+    {
+        DrawFieldOfView();
+    }
+
+
+    IEnumerator FindPlayerWithDelay(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            FindVisiblePlayer();
+        }
+    }
+
+    void FindVisiblePlayer()
+    {
+        Collider[] playerInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, playerMask);
+        for (int i = 0; i < playerInViewRadius.Length; i++)
+        {
+            Transform player = playerInViewRadius[i].transform;
+            Vector3 dirToPlayer = (player.position - transform.position).normalized;
+            if (Vector3.Angle(transform.forward, dirToPlayer) < viewAngle / 2)
+            {
+                float distToPlayer = Vector3.Distance(transform.position, player.position);
+                if (!Physics.Raycast(transform.position, dirToPlayer, distToPlayer, obstacleMask))
+                {
+                    //Detected
+                    print("Detected");
+                }
+            }
+        }
+    }
+
+    public Vector3 DirFromAngle(float angleDegrees, bool globalAngle)
+    {
+        if (!globalAngle)
+        {
+            angleDegrees += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleDegrees * Mathf.Deg2Rad));
+    }
+
+    void DrawFieldOfView()
+    {
+        int rayCount = Mathf.RoundToInt(viewAngle * meshResolution);
+        float rayAngleSize = viewAngle / rayCount;
+
+        List<Vector3> viewPoints = new List<Vector3>();
+        ViewCastInfo oldViewCast = new ViewCastInfo();
+
+        for (int i = 0; i <= rayCount; i++)
+        {
+            float angle = transform.eulerAngles.y - viewAngle / 2 + rayAngleSize * i;
+            ViewCastInfo newViewCast = ViewCast(angle);
+            if (i > 0)
+            {
+                bool edgeDstThresholdExceeded = Mathf.Abs(oldViewCast.dst - newViewCast.dst) > edgeDstThreshold;
+                if (oldViewCast.hit != newViewCast.hit || (oldViewCast.hit && newViewCast.hit && edgeDstThresholdExceeded))
+                {
+                    EdgeInfo edge = FindEdge(oldViewCast, newViewCast);
+                    if (edge.pointA != Vector3.zero)
+                    {
+                        viewPoints.Add(edge.pointA);
+                    }
+                    if (edge.pointB != Vector3.zero)
+                    {
+                        viewPoints.Add(edge.pointB);
+                    }
+                }
+            }
+
+            viewPoints.Add(newViewCast.point);
+            oldViewCast = newViewCast;
+        }
+
+        int vertexCount = viewPoints.Count + 1;
+        Vector3[] verticies = new Vector3[vertexCount];
+        int[] triangles = new int[(vertexCount - 2) * 3];
+
+        verticies[0] = Vector3.zero;
+        for (int i = 0; i < vertexCount - 1; i++)
+        {
+            verticies[i + 1] = transform.InverseTransformPoint(viewPoints[i]);
+            if (i < vertexCount - 2)
+            {
+                triangles[i * 3] = 0;
+                triangles[i * 3 + 1] = i + 1;
+                triangles[i * 3 + 2] = i + 2;
+            }
+        }
+
+        viewMesh.Clear();
+        viewMesh.vertices = verticies;
+        viewMesh.triangles = triangles;
+        viewMesh.RecalculateNormals();
+    }
+
+
+
+    ViewCastInfo ViewCast(float globalAngle)
+    {
+        Vector3 dir = DirFromAngle(globalAngle, true);
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, dir, out hit, viewRadius, obstacleMask))
+        {
+            return new ViewCastInfo(true, hit.point, hit.distance, globalAngle);
+        }
+        else
+        {
+            return new ViewCastInfo(false, transform.position + dir * viewRadius, viewRadius, globalAngle);
+        }
+    }
+
+    EdgeInfo FindEdge(ViewCastInfo minViewCast, ViewCastInfo maxViewCast)
+    {
+        float minAngle = minViewCast.angle;
+        float maxAngle = maxViewCast.angle;
+
+        Vector3 minPoint = Vector3.zero;
+        Vector3 maxPoint = Vector3.zero;
+
+        for (int i = 0; i < edgeResolveIterations; i++)
+        {
+            float angle = (minAngle + maxAngle) / 2;
+            ViewCastInfo newViewCast = ViewCast(angle);
+            bool edgeDstThresholdExceeded = Mathf.Abs(minViewCast.dst - newViewCast.dst) > edgeDstThreshold;
+            if (newViewCast.hit == minViewCast.hit && !edgeDstThresholdExceeded)
+            {
+                minAngle = angle;
+                minPoint = newViewCast.point;
+            }
+            else
+            {
+                maxAngle = angle;
+                maxPoint = newViewCast.point;
+            }
+        }
+
+        return new EdgeInfo(minPoint, maxPoint);
+    }
+
+    public struct ViewCastInfo
+    {
+        public bool hit;
+        public Vector3 point;
+        public float dst;
+        public float angle;
+
+        public ViewCastInfo(bool hit, Vector3 point, float dst, float angle)
+        {
+            this.hit = hit;
+            this.point = point;
+            this.dst = dst;
+            this.angle = angle;
+        }
+    }
+
+    public struct EdgeInfo
+    {
+        public Vector3 pointA;
+        public Vector3 pointB;
+
+        public EdgeInfo(Vector3 pointA, Vector3 pointB)
+        {
+            this.pointA = pointA;
+            this.pointB = pointB;
+        }
+    }
+}
---
+++ BStealth/Assets/Scripts/FoVEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof(EnemySight))]
+public class FoVEditor : Editor {
+
+    private void OnSceneGUI()
+    {
+        EnemySight fow = (EnemySight)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(fow.transform.position,Vector3.up,Vector3.forward,360,fow.viewRadius);
+        Vector3 viewAngleA = fow.DirFromAngle(-fow.viewAngle / 2, false);
+        Vector3 viewAngleB = fow.DirFromAngle(fow.viewAngle / 2, false);
+
+        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleA * fow.viewRadius);
+        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleB * fow.viewRadius);
+
+    }
+}
