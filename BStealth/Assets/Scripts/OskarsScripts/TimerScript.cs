3c8f36aa0cae1c5012175238c77eb8e7235e994e
OskarMagnusson <oskar.magnusson97@gmail.com>
2018-12-12 20:11:30 +0100

    Timer
    Finns en timer nu

--- BStealth/Assets/Scripts/OskarsScripts/TimerScript.cs
+++ BStealth/Assets/Scripts/OskarsScripts/TimerScript.cs
@@ -1,16 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimerScript : MonoBehaviour {
 
+    [SerializeField]
+    Text textUI; 
+    [SerializeField]
+    float time; //How many secounds the timer is
+
 	// Use this for initialization
 	void Start () {
-		
+        StartCoroutine(TheTimer());
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    IEnumerator TheTimer()
+    {
+        for(float f = time; f >= 0; f -= (1f/60f))
+        {
+            textUI.text = f.ToString("F"); //Changes the ui text to the current time
+            yield return null;
+        }
+        textUI.text = "0.00"; //timer sometimes ends att 0.02 so this just changes the text to always be 0.00
+        print("time is upp boi"); //executes after timer 
+    }
 }
