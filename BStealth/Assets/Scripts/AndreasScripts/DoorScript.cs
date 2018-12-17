8d6949c98c646d5986cd194cf356d5b188d3a1c0
Andreas Halldin <iqed@live.se>
2018-12-17 14:19:56 +0100

    Correct rotation on UI for doors

--- BStealth/Assets/Scripts/AndreasScripts/DoorScript.cs
+++ BStealth/Assets/Scripts/AndreasScripts/DoorScript.cs
@@ -12,8 +12,20 @@ public class DoorScript : MonoBehaviour
     public Slider unlockProgress;
     public bool aquiredKey;
 
+    public List<GameObject> uiElements = new List<GameObject>();
 
 
+
+    private void Start()
+    {
+        foreach (GameObject uiElement in uiElements)
+        {
+            uiElement.transform.rotation = Quaternion.Euler(90, 90, transform.rotation.y + 90);
+        }
+        //canvasUnlocking.transform.rotation = Quaternion.Euler(90, 0, 90);
+        //canvasKey.transform.rotation = Quaternion.Euler(90, 0, 90);
+    }
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
