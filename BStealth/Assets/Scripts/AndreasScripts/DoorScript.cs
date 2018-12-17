7bdc82c3246d496a558aa619ade367438f1c4b51
Andreas Halldin <iqed@live.se>
2018-12-17 12:47:41 +0100

    Fixed New Healthbar, Doors and Keys added

---
+++ BStealth/Assets/Scripts/AndreasScripts/DoorScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorScript : MonoBehaviour
+{
+
+    bool unlocking = false;
+    public GameObject canvas;
+    public Slider unlockProgress;
+    public bool aquiredKey;
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            unlocking = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            unlocking = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (!aquiredKey)
+        {
+            return;
+        }
+        if (unlocking)
+        {
+            unlockProgress.value++;
+            if (unlockProgress.value == unlockProgress.maxValue)
+            {
+                Destroy(gameObject);
+            }
+        }
+        else
+        {
+            unlockProgress.value = 0;
+        }
+        canvas.SetActive(unlocking);
+        
+    }
+
+}
---
+++ BStealth/Assets/Scripts/AndreasScripts/KeyScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyScript : MonoBehaviour {
+
+    public DoorScript door;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            door.aquiredKey = true;
+            Destroy(gameObject);
+        }
+    }
+}
