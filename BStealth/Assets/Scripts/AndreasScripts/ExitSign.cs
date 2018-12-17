060ac6a5cee34d198947cb86d00f173ae5c7749e
Andreas Halldin <iqed@live.se>
2018-12-17 09:53:58 +0100

    Exit Sign

---
+++ BStealth/Assets/Scripts/AndreasScripts/ExitSign.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitSign : MonoBehaviour
+{
+
+    public string nextSceneName;
+    public int nextSceneID;
+    public bool switchUsingID;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (switchUsingID)
+            {
+                SceneManager.LoadScene(nextSceneID);
+
+            }
+            else
+            {
+                SceneManager.LoadScene(nextSceneName);
+            }
+        }
+    }
+}
