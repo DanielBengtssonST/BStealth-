0370c0ec96959c6cc8fb02a38d4dc40083b46a93
Andreas Halldin <iqed@live.se>
2018-12-20 22:14:44 +0100

    Dörrar

--- BStealth/Assets/Scripts/AndreasScripts/DoorScript.cs
+++ BStealth/Assets/Scripts/AndreasScripts/DoorScript.cs
@@ -18,10 +18,10 @@ public class DoorScript : MonoBehaviour
 
     private void Start()
     {
-        foreach (GameObject uiElement in uiElements)
-        {
-            uiElement.transform.rotation = Quaternion.Euler(90, 90, transform.rotation.y + 90);
-        }
+        //foreach (GameObject uiElement in uiElements)
+        //{
+        //    uiElement.transform.localRotation = Quaternion.Euler(90, 90, transform.rotation.y + 90);
+        //}
         //canvasUnlocking.transform.rotation = Quaternion.Euler(90, 0, 90);
         //canvasKey.transform.rotation = Quaternion.Euler(90, 0, 90);
     }
