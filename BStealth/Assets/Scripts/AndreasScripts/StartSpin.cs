f770fec536cbf44803ccf29886cddc83699da2ff
Andreas Halldin <iqed@live.se>
2018-12-26 16:10:22 +0100

    Added scenes for level 3 and 4

---
+++ BStealth/Assets/Scripts/AndreasScripts/StartSpin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSpin : MonoBehaviour
+{
+    public SimpleRotation rotation;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            rotation.rotating = true;
+        }
+    }
+}
