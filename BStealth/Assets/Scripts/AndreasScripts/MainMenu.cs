2d38fc1999cbff825646f7d5929a1d05b0f3332a
Andreas Halldin <iqed@live.se>
2018-12-30 14:50:28 +0100

    Main Menu Done

---
+++ BStealth/Assets/Scripts/AndreasScripts/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public GameObject controls, main;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Controls()
+    {
+        controls.SetActive(true);
+        main.SetActive(false);
+    }
+
+    public void HardcoreLevel3()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Back()
+    {
+        controls.SetActive(false);
+        main.SetActive(true);
+    }
+}
